d86eaae99bb3462e458054e7f7998f69c06cf743
Martino Fregona <martinofregona@MacBook-Pro-Gianluca.local>
2022-06-13 08:45:06 +0200

    Aggiunto campo immagine per i montanti da client

--- configurator/src/components/controls/AddUpright.vue
+++ configurator/src/components/controls/AddUpright.vue
@@ -2,7 +2,7 @@
   <div uprights v-if="productOptions && uprights.length" class="grid grid-cols-3 gap-8 w-full">
     <div v-for="(upright, index) in uprights" :key="`upright-${index}`" class="cursor-pointer hover:opacity-60 transition-all duration-400 group">
       <div upright @click="configurator.addElement({ ...upright.variants[0], id: upright.id, variantId: upright.variants[0].id })">
-        <img v-if="upright.image" :src="upright.image.url" :width="upright.image.width" :height="upright.image.height" class="w-16 h-16 object-cover p-4 transform duration-400 transition-all group-hover:-translate-y-2" :alt="upright.name">
+        <img v-if="upright.image" :src="upright.image.url" :width="upright.image.width" :height="upright.image.height" class="w-32 h-32 object-contain mx-auto pb-4 transform duration-400 transition-all group-hover:-translate-y-2" :alt="upright.name">
         <img v-else src="https://placehold.jp/150x150.png" width="150" height="150" alt="" class="bg-light-gray w-24 h-24 object-cover mx-auto mb-4"/>
         <div class="text-center" v-text="upright.name" />
       </div>
