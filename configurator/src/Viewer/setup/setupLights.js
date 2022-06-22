5df7505db4200c0932e24c3e8ae11d36902fbbf1
Martino Fregona <martinofregona@MacBook-Pro-Gianluca.local>
2022-06-22 12:09:13 +0200

    I montanti K2 si adattano alla stanza, i piedini vengono ruotati con una configurazione fissa in base all'inclinazione del soffitto

--- configurator/src/Viewer/setup/setupLights.js
+++ configurator/src/Viewer/setup/setupLights.js
@@ -4,10 +4,10 @@ import { FLOOR_DEPTH } from '@/dataset/defaultConfiguratorValues'
 export const setupLights = ({ width, height }) => {
   const ambientLight = new THREE.AmbientLight( 0xffffff, 0.6 )
 
-  const spotLight = new THREE.SpotLight( 0xffffff, 0.8, 0, Math.PI / 2 )
-  spotLight.position.set( width / 2, height, FLOOR_DEPTH )
+  const spotLight = new THREE.SpotLight( 0xffffff, 0.7, 0, Math.PI / 2 )
+  spotLight.position.set(width / 2, 5, FLOOR_DEPTH)
+  spotLight.target.position.set(width / 2, 5, 0)
   spotLight.castShadow = true
-  spotLight.target.position.set(width / 2, height / 2, 0)
   spotLight.shadow.mapSize.width = 1024
   spotLight.shadow.mapSize.height = 1024
   spotLight.shadow.camera.near = 0.5
@@ -17,8 +17,8 @@ export const setupLights = ({ width, height }) => {
 
   const spotLight2 = new THREE.SpotLight( 0xffffff, 0.05, 0, Math.PI / 2 )
   spotLight2.position.set(width / 6, height / 2, FLOOR_DEPTH )
-  spotLight2.castShadow = true
   spotLight2.target.position.set(width / 6, height / 2, 0)
+  spotLight2.castShadow = true
   spotLight2.shadow.mapSize.width = 1024
   spotLight2.shadow.mapSize.height = 1024
   spotLight2.shadow.camera.near = 0.5
@@ -37,8 +37,21 @@ export const setupLights = ({ width, height }) => {
   spotLight3.shadow.focus = 10
   spotLight3.shadow.bias = 0.0001
 
-  const helper2 = new THREE.SpotLightHelper(spotLight2)
-  const helper3 = new THREE.SpotLightHelper(spotLight3)
+  const spotLight4 = new THREE.SpotLight( 0xffffff, 0.5, 0, Math.PI / 2 )
+  spotLight4.position.set(width / 2, 0, FLOOR_DEPTH / 2.5)
+  spotLight4.target.position.set(width / 2, height / 2, 0)
+  spotLight4.castShadow = true
+  spotLight4.shadow.mapSize.width = 1024
+  spotLight4.shadow.mapSize.height = 1024
+  spotLight4.shadow.camera.near = 0.5
+  spotLight4.shadow.camera.far = 500
+  spotLight4.shadow.focus = 10
+  spotLight4.shadow.bias = 0.0001
+
+  const helper = new THREE.SpotLightHelper(spotLight, 0xff0000)
+  const helper2 = new THREE.SpotLightHelper(spotLight2, 0xff0000)
+  const helper3 = new THREE.SpotLightHelper(spotLight3, 0xff0000)
+  const helper4 = new THREE.SpotLightHelper(spotLight4, 0x0000ff)
 
   const pointLight = new THREE.PointLight(0xffffff, 0.75, 0, 1)
 
@@ -47,14 +60,8 @@ export const setupLights = ({ width, height }) => {
   const rectAreaLight = new THREE.RectAreaLight(0xffffff, 0.4, width, FLOOR_DEPTH)
   rectAreaLight.position.set(width / 2, height - 0.1, FLOOR_DEPTH / 2)
   rectAreaLight.lookAt(width / 2, height, FLOOR_DEPTH / 2)
-  return [
-    ambientLight,
-    //spotLight,
-    spotLight2,
-    spotLight3,
-    //helper2,
-    //helper3,
-    pointLight,
-    rectAreaLight
-  ]
+  return {
+    classic: [ambientLight, /*spotLight*/, spotLight2, spotLight3, /*helper2*/, /*helper3*/, pointLight, rectAreaLight ],
+    attic: [ambientLight, /*spotLight*/, spotLight2, spotLight3, spotLight4, /*helper*/, /*helper2*/, /*helper3*/, pointLight, rectAreaLight ]
+  }
 }
