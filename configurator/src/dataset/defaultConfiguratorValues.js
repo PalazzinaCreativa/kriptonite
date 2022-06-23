c9d2c4b64098bf7c1145acc38a5cf81d1d5ea015
Martino Fregona <martinofregona@MacBook-Pro-Gianluca.local>
2022-06-23 17:32:51 +0200

    Gutter globale a 20

--- configurator/src/dataset/defaultConfiguratorValues.js
+++ configurator/src/dataset/defaultConfiguratorValues.js
@@ -4,6 +4,6 @@ export const STANDALONE_Z = 200 // In che punto z posizionare gli elementi in me
 
 export const NICHE_PADDING = 100 // Spazio esterno della nicchia
 
-export const GUTTER = 15 // Il prodotto non può essere posizionato a filo con la parete
+export const GUTTER = 20 //15 // Il prodotto non può essere posizionato a filo con la parete
 
 export const RESTING_ON_THE_GROUND = ['sofa', 'table', 'door', 'upright_s_tele', 'upright_s'] // Funziona solamente per i set di dati mock. Da oggi utilizziamo il campo 'grounded' a DB 
