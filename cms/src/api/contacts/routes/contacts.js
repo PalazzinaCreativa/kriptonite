1e59d0d6ff9e10f2e5150a9044bc5bb8d0e7a33d
davideluchetta <davide@palazzinacreativa.it>
2022-07-13 15:15:30 +0200

    added contact type

---
+++ cms/src/api/contacts/routes/contacts.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/contacts',
+    //  handler: 'contacts.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
---
+++ cms/src/api/contacts/services/contacts.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * contacts service.
+ */
+
+module.exports = () => ({});
