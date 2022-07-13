122817eea68b333e9c2cfff203186b3f8c672766
Martino Fregona <martinofregona@mbp-gianluca.lan>
2022-07-13 10:29:31 +0200

    Card verticali

--- configurator/src/components/QuestionCard.vue
+++ configurator/src/components/QuestionCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex gap-12 items-center" v-if="isVisible">
-    <div class="flex w-full bg-white group relative my-4 min-h-[200px] cursor-pointer" ref="button">
+    <div class="flex w-full bg-white group relative my-4 min-h-[200px] cursor-pointer" :class="props.option.description ? 'flex-nowrap' : 'flex-wrap'" ref="button">
       <div v-if="props.option.image" class="w-full">
-        <img class="h-full object-cover w-full" :src="props.option.image" width="200" height="200" :alt="props.option.key"/>
+        <img class="h-full object-cover w-full" :class="props.option.description ? '' : 'max-h-[200px]'" :src="props.option.image" width="200" height="200" :alt="props.option.key"/>
       </div>
-      <div class="flex flex-wrap py-10 px-6 w-full">
-        <div class="font-regular text-black text-m mb-4 w-full">{{ props.option.label }}<slot /></div>
+      <div class="flex flex-wrap px-6 gap-4 w-full" :class="props.option.description ? 'py-10' : 'py-6'">
+        <div class="font-regular text-black text-m w-full">{{ props.option.label }}<slot /></div>
         <div v-if="props.option.description" class="text-xs font-light w-full" v-html="props.option.description" />
       </div>
     </div>
